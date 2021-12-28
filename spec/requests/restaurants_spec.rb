6d3db91c3fdb4b0ba1b85924cdfb6d7b38ce33d3
Josh Justice <me@codingitwrong.com>
2021-12-28 10:32:47 -0500

    Group tests for more cases

--- spec/requests/restaurants_spec.rb
+++ spec/requests/restaurants_spec.rb
@@ -4,31 +4,39 @@ RSpec.describe 'Restaurants', type: :request do
   let(:api_key) { 'ABC123' }
   let!(:client_app) { ClientApp.create!(apiKey: api_key) }
 
-  it 'allows retrieving all restaurants' do
-    client_app.restaurants.create!(name: 'Pizza Place')
-    client_app.restaurants.create!(name: 'Salad Place')
+  describe '#index' do
+    context 'with a valid API key' do
+      it 'returns all restaurants for that key' do
+        client_app.restaurants.create!(name: 'Pizza Place')
+        client_app.restaurants.create!(name: 'Salad Place')
 
-    get "/#{api_key}/restaurants"
+        get "/#{api_key}/restaurants"
 
-    expect(response.status).to eql(200)
+        expect(response.status).to eql(200)
 
-    response_body = JSON.parse(response.body)
-    expect(response_body.count).to eql(2)
-    expect(response_body[0]['name']).to eql('Pizza Place')
-    expect(response_body[1]['name']).to eql('Salad Place')
+        response_body = JSON.parse(response.body)
+        expect(response_body.count).to eql(2)
+        expect(response_body[0]['name']).to eql('Pizza Place')
+        expect(response_body[1]['name']).to eql('Salad Place')
+      end
+    end
   end
 
-  it 'allows creating a restaurant' do
-    headers = {'Content-Type' => 'application/json'}
+  describe '#create' do
+    context 'with a valid API key' do
+      it 'saves and returns the restaurant' do
+        headers = {'Content-Type' => 'application/json'}
 
-    name = 'Burger Place'
-    body = { name: name }
+        name = 'Burger Place'
+        body = { name: name }
 
-    post "/#{api_key}/restaurants", headers: headers, params: body.to_json
+        post "/#{api_key}/restaurants", headers: headers, params: body.to_json
 
-    expect(response.status).to eq(200)
+        expect(response.status).to eq(200)
 
-    response_body = JSON.parse(response.body)
-    expect(response_body['name']).to eql(name)
+        response_body = JSON.parse(response.body)
+        expect(response_body['name']).to eql(name)
+      end
+    end
   end
 end
