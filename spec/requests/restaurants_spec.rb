126aa84438d115cb6f7ff5aab59a605db4db7e3f
Josh Justice <me@codingitwrong.com>
2021-12-28 10:38:48 -0500

    Test that all expected fields, and no others, are returned

--- spec/requests/restaurants_spec.rb
+++ spec/requests/restaurants_spec.rb
@@ -4,20 +4,34 @@ RSpec.describe 'Restaurants', type: :request do
   let(:api_key) { 'ABC123' }
   let!(:client_app) { ClientApp.create!(apiKey: api_key) }
 
+  def date_to_string(date) = date.iso8601(3)
+
   describe '#index' do
     context 'with a valid API key' do
       it 'returns all restaurants for that key' do
-        client_app.restaurants.create!(name: 'Pizza Place')
-        client_app.restaurants.create!(name: 'Salad Place')
+        restaurant1 = client_app.restaurants.create!(name: 'Pizza Place')
+        restaurant2 = client_app.restaurants.create!(name: 'Salad Place')
 
         get "/#{api_key}/restaurants"
 
         expect(response.status).to eql(200)
 
         response_body = JSON.parse(response.body)
-        expect(response_body.count).to eql(2)
-        expect(response_body[0]['name']).to eql('Pizza Place')
-        expect(response_body[1]['name']).to eql('Salad Place')
+
+        expect(response_body).to eq([
+          {
+            'id' => restaurant1.id,
+            'name' => restaurant1.name,
+            'created_at' => date_to_string(restaurant1.created_at),
+            'updated_at' => date_to_string(restaurant1.updated_at),
+          },
+          {
+            'id' => restaurant2.id,
+            'name' => restaurant2.name,
+            'created_at' => date_to_string(restaurant2.created_at),
+            'updated_at' => date_to_string(restaurant2.updated_at),
+          },
+        ])
       end
     end
   end
@@ -30,12 +44,22 @@ RSpec.describe 'Restaurants', type: :request do
         name = 'Burger Place'
         body = { name: name }
 
-        post "/#{api_key}/restaurants", headers: headers, params: body.to_json
+        expect {
+          post "/#{api_key}/restaurants", headers: headers, params: body.to_json
+        }.to change {Restaurant.count}.by(1)
+
+        restaurant = Restaurant.last
+        expect(restaurant.name).to eq(name)
 
         expect(response.status).to eq(200)
 
         response_body = JSON.parse(response.body)
-        expect(response_body['name']).to eql(name)
+        expect(response_body).to eq(
+          'id' => restaurant.id,
+          'name' => name,
+          'created_at' => date_to_string(restaurant.created_at),
+          'updated_at' => date_to_string(restaurant.updated_at),
+        )
       end
     end
   end
