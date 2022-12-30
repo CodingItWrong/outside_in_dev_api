a7d38602702a85997aa4d8136718ff59336b9f22
Josh Justice <me@codingitwrong.com>
2022-12-30 14:24:22 -0500

    Format to Standard

--- spec/requests/restaurants_spec.rb
+++ spec/requests/restaurants_spec.rb
@@ -1,27 +1,27 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe 'Restaurants', type: :request do
-  let(:api_key) { 'ABC123' }
+RSpec.describe "Restaurants", type: :request do
+  let(:api_key) { "ABC123" }
   let!(:client_app) { ClientApp.create!(apiKey: api_key) }
-  let!(:other_client_app) { ClientApp.create!(apiKey: 'other_key') }
-  let!(:restaurant1) { client_app.restaurants.create!(name: 'Pizza Place') }
-  let!(:restaurant2) { client_app.restaurants.create!(name: 'Salad Place') }
-  let!(:other_restaurant) { other_client_app.restaurants.create!(name: 'Other Place') }
+  let!(:other_client_app) { ClientApp.create!(apiKey: "other_key") }
+  let!(:restaurant1) { client_app.restaurants.create!(name: "Pizza Place") }
+  let!(:restaurant2) { client_app.restaurants.create!(name: "Salad Place") }
+  let!(:other_restaurant) { other_client_app.restaurants.create!(name: "Other Place") }
 
   def date_to_string(date) = date.iso8601(3)
 
-  describe '#index' do
-    context 'with an invalid API key' do
-      it 'returns not found' do
-        get '/invalid_api_key/restaurants'
+  describe "#index" do
+    context "with an invalid API key" do
+      it "returns not found" do
+        get "/invalid_api_key/restaurants"
         expect(response.status).to eq(404)
       end
     end
 
-    context 'with a valid API key' do
-      it 'returns all restaurants for that key' do
+    context "with a valid API key" do
+      it "returns all restaurants for that key" do
         get "/#{api_key}/restaurants"
 
         expect(response.status).to eql(200)
@@ -29,34 +29,34 @@ RSpec.describe 'Restaurants', type: :request do
         response_body = JSON.parse(response.body)
 
         expect(response_body).to eq([
-                                      {
-                                        'id' => restaurant1.id,
-                                        'name' => restaurant1.name
-                                      },
-                                      {
-                                        'id' => restaurant2.id,
-                                        'name' => restaurant2.name
-                                      },
-                                    ])
+          {
+            "id" => restaurant1.id,
+            "name" => restaurant1.name
+          },
+          {
+            "id" => restaurant2.id,
+            "name" => restaurant2.name
+          }
+        ])
       end
     end
   end
 
-  describe '#create' do
-    name = 'Burger Place'
-    body = { name: }
-    headers = { 'Content-Type' => 'application/json' }
+  describe "#create" do
+    name = "Burger Place"
+    body = {name:}
+    headers = {"Content-Type" => "application/json"}
 
-    context 'with an invalid API key' do
-      it 'returns not found' do
+    context "with an invalid API key" do
+      it "returns not found" do
         expect {
-          post '/invalid_api_key/restaurants', headers:, params: body.to_json
+          post "/invalid_api_key/restaurants", headers:, params: body.to_json
         }.not_to(change { Restaurant.count })
       end
     end
 
-    context 'with a valid API key' do
-      it 'saves and returns the restaurant' do
+    context "with a valid API key" do
+      it "saves and returns the restaurant" do
         expect {
           post "/#{api_key}/restaurants", headers:, params: body.to_json
         }.to change { Restaurant.count }.by(1)
@@ -68,13 +68,13 @@ RSpec.describe 'Restaurants', type: :request do
 
         response_body = JSON.parse(response.body)
         expect(response_body).to eq(
-          'id' => restaurant.id,
-          'name' => name,
+          "id" => restaurant.id,
+          "name" => name
         )
       end
 
-      it 'rejects invalid input' do
-        invalid_body = { name: '' }
+      it "rejects invalid input" do
+        invalid_body = {name: ""}
         expect {
           post "/#{api_key}/restaurants", headers:, params: invalid_body.to_json
         }.not_to(change { Restaurant.count })
@@ -83,31 +83,31 @@ RSpec.describe 'Restaurants', type: :request do
 
         response_body = JSON.parse(response.body)
         expect(response_body).to eq(
-          'name' => ["can't be blank"],
+          "name" => ["can't be blank"]
         )
       end
     end
   end
 
-  describe '#destroy' do
+  describe "#destroy" do
     let!(:restaurant) { FactoryBot.create(:restaurant) }
 
-    context 'with an invalid API key' do
-      it 'returns not found' do
+    context "with an invalid API key" do
+      it "returns not found" do
         expect {
           delete "/invalid_api_key/restaurants/#{restaurant.id}", headers:
         }.not_to(change { Restaurant.count })
       end
     end
 
-    context 'with a valid API key' do
-      it 'deletes the restaurant' do
+    context "with a valid API key" do
+      it "deletes the restaurant" do
         expect {
           delete "/#{restaurant.application.apiKey}/restaurants/#{restaurant.id}", headers:
         }.to change { Restaurant.count }.by(-1)
 
         expect(response.status).to eq(204)
-        expect(response.body).to eq('')
+        expect(response.body).to eq("")
       end
     end
   end
--- spec/system/client_apps_spec.rb
+++ spec/system/client_apps_spec.rb
@@ -1,23 +1,23 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe 'ClientApps', type: :system do
+RSpec.describe "ClientApps", type: :system do
   before do
     driven_by(:rack_test)
   end
 
-  it 'allows creating a client app' do
-    api_key = 'ABC123'
+  it "allows creating a client app" do
+    api_key = "ABC123"
 
     allow(SecureRandom).to receive(:alphanumeric)
       .with(32)
       .and_return(api_key)
 
-    visit '/'
-    click_on 'Create API Key'
+    visit "/"
+    click_on "Create API Key"
 
-    expect(page).to have_selector('.api-key')
+    expect(page).to have_selector(".api-key")
     expect(page).to have_selector(".api-key[value=#{api_key}]")
 
     expect(ClientApp.count).to eql(1)
