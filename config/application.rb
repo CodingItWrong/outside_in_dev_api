af4127a7a35059db496df561f3ad30a92ba894c9
Josh Justice <me@codingitwrong.com>
2023-11-03 06:18:23 -0500

    Fix error and warning

--- config/application.rb
+++ config/application.rb
@@ -34,7 +34,5 @@ module OutsideInDevApi
 
     # Don't generate system test files.
     config.generators.system_tests = nil
-
-    config.active_record.legacy_connection_handling = false
   end
 end
